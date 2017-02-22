d2dea42b02c7c6ce9d97dc0a4caf1cce20b36486
Julian <jinwood8@outlook.com>
2017-02-22 18:55:54 +0000

    Switch to use a md parser that supports md metadata. Sort posts on date created.

--- blog.js
+++ blog.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
-var md = require('markdown-js');
+var metaMarked = require('meta-marked');
 
 function _readFiles(dirname, posts, onFileContent, onComplete){
     fs.readdir(dirname, function(err, fileNames){
         var count = fileNames.length;
         fileNames.forEach(function(fileName){
             fs.readFile(dirname + fileName, 'utf-8', function(err, content){
-                posts.push(md.makeHtml(content));
-                onFileContent(posts, count);
+                posts.push(metaMarked(content));
+                if(posts.length === count)
+                    onFileContent(posts);
             });
         });
     });
@@ -17,10 +18,14 @@ function _readFiles(dirname, posts, onFileContent, onComplete){
 
 var getAllPosts = function(res, cb){
     var posts = [];
-    _readFiles('./posts/', posts, function(posts, count){
-        if (posts.length === count)
-            cb(res, posts);
+    _readFiles('./posts/', posts, function(posts){
+        posts = posts.sort(function(a,b){
+            a = new Date(a.meta.DateCreated);
+            b = new Date(b.meta.DateCreated);
+            return a>b ? -1 : a<b ? 1 : 0; //newest first
+        });
+        cb(res, posts);
     });
-}
+};
 
 exports.getAllPosts = getAllPosts;
